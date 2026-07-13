98857f16df1bc124501c23213b80a5128e514b9a
Saint Jameson Parid <saintparid@gmail.com>
2026-07-13 21:06:20 +0700

    Add movie and book data variables
    Initialized variables for favorite movie and book details.

---
+++ variabel.js
@@ -0,0 +1,14 @@
+// Task 1: Data film favorit Steve
+let movieTitle = "Captain Marvel";
+let movieRating = 6.8;
+let yearOfIssue = 2019;
+let forAllAges = false;
+
+// Task 2: Data buku The Quantum Universe
+let bookTitle = "The Quantum Universe";
+let authorName = "Brian Cox and Jeff Forshaw";
+let yearOfPublication = 2011;
+let isScience = true;
+
+// Menampilkan nilai variabel buku di console
+console.log(bookTitle, authorName, yearOfPublication, isScience);
